b5bc7251fbe4e24a916ffddef366c6d6284afe65
Mohammad-Nadil <mdnadil50@gmail.com>
2026-03-11 14:54:59 +0600

    banner created

--- app/(home)/layout.jsx
+++ app/(home)/layout.jsx
@@ -3,11 +3,5 @@ import Navbar from "@/components/Navbar";
 import React from "react";
 
 export default function layout({ children }) {
-  return (
-    <div>
-      <Navbar />
-      {children}
-      <Footer />
-    </div>
-  );
+  return <div>{children}</div>;
 }
--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -1,8 +1,11 @@
+import Banner from '@/components/home/Banner'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <Banner/>
+    </div>
   )
 }
 
