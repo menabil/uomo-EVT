fe366d3b7aed00e2979d8b7fa2ea96ff0c23b11d
menabil <nabil99cc@gmail.com>
2026-03-12 19:12:59 +0600

    responsive checkout page

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${jost.variable} antialiased`}>
-        <Navbar />
+        {/* <Navbar /> */}
         {children}
         <Footer />
       </body>
