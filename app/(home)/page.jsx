1933cffe7b2377af615e5d53d9b77947d64b7842
mohammad-nadil <mdhossain.dev@gmail.com>
2026-03-13 17:12:22 +0600

    home structure created

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -1,12 +1,20 @@
-import Banner from '@/components/home/Banner'
-import React from 'react'
+import Arrival from "@/components/home/Arrival";
+import Banner from "@/components/home/Banner";
+import Category from "@/components/home/Category";
+import InstaHome from "@/components/home/InstaHome";
+import Newsletter from "@/components/home/Newsletter";
+import React from "react";
 
 const page = () => {
   return (
     <div>
-      <Banner/>
+      <Banner />
+      <Category />
+      <Arrival />
+      <Newsletter />
+      <InstaHome />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
---
+++ components/home/InstaHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InstaHome = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default InstaHome
